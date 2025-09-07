262eed8c4499c1574403ea4c2ceed9ee7fd3d6f5
Bethel-21 <bethelhemdanielfenta@gmail.com>
2025-09-07 10:17:27 +0300

    minor change

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,9 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name)
+
 books_by_author = Book.objects.filter(author=author)
 print("Books by", author.name, ":", list(books_by_author))
 
@@ -15,5 +17,8 @@ library_books = library.books.all()
 print("Books in", library.name, ":", list(library_books))
 
 # Retrieve the librarian for a library
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 librarian = Librarian.objects.get(library=library)
+
 print("Librarian of", library.name, ":", librarian.name)
