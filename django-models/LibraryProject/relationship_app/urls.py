7887da271653423744926b3fd8c47ef2ac78fb06
Bethel-21 <bethelhemdanielfenta@gmail.com>
2025-09-07 12:27:42 +0300

    change

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("books/", views.list_books, name="list_books"),
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
     path("register/", views.register_view, name="register"),  # keep custom registration
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
