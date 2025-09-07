fe60dad4a2f6904ef5bf4f035f13e7691f424de6
Bethel-21 <bethelhemdanielfenta@gmail.com>
2025-09-07 10:13:32 +0300

    minor change

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,7 +8,9 @@ books_by_author = Book.objects.filter(author=author)
 print("Books by", author.name, ":", list(books_by_author))
 
 # List all books in a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+
 library_books = library.books.all()
 print("Books in", library.name, ":", list(library_books))
 
