4fae05dc456d9ce9aebe7081126714194f20cdfc
Bethel-21 <bethelhemdanielfenta@gmail.com>
2025-09-07 10:25:27 +0300

    add template

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.detail import DetailView
+from .models import Book, Library
 
-# Create your views here.
+
+# ---------------------------
+# Function-based view
+# ---------------------------
+def list_books(request):
+    books = Book.objects.all()
+    
+    # Plain text output (or switch to template if you prefer)
+    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(output, content_type="text/plain")
+
+    # Optional template version:
+    # return render(request, "list_books.html", {"books": books})
+
+
+# ---------------------------
+# Class-based view
+# ---------------------------
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"  # uses your template
+    context_object_name = "library"        # name in template
+
