25aa73a9799c3495ae362e8cf9695f06e0ee3239
Bethel-21 <bethelhemdanielfenta@gmail.com>
2025-09-07 11:54:59 +0300

    template addition

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,13 @@
 # relationship_app/urls.py
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from .views import list_books
 
 urlpatterns = [
-    path("books/", views.list_books, name="list_books"),  # FBV
-    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # CBV
-    ath("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+    path("", views.home_view, name="home"),
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+    path("register/", views.register_view, name="register"),  # keep custom registration
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 ]
