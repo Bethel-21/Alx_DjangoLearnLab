8e966d32c2181d4480826db37dc116e9055d0e19
Bethel-21 <bethelhemdanielfenta@gmail.com>
2025-09-21 21:52:38 +0300

    add authentication

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # 👈 include API app routes
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
 ]
