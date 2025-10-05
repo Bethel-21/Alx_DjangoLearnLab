15bc06654a2e5ded285c0a2ae7d5b0102d02de86
Bethel-21 <bethelhemdanielfenta@gmail.com>
2025-10-05 16:56:28 +0300

    change

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment, Tag
+from taggit.forms import TagWidget
+
 
 # Custom registration form
 class CustomUserCreationForm(UserCreationForm):
