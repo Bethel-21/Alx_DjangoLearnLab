6edeb3622479dd3c12669734c9f07c01be073b1c
Bethel-21 <bethelhemdanielfenta@gmail.com>
2025-10-11 19:47:23 +0300

    change

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,28 +1,22 @@
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from django.contrib.auth import authenticate
 
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
-    profile_picture = serializers.ImageField(required=False, allow_null=True)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        # Use the model's create_user to handle password hashing
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', '')
-        )
+        user = User.objects.create_user(**validated_data)  # use create_user
+        Token.objects.create(user=user)  # create a token
         return user
+
 
 
 class UserLoginSerializer(serializers.Serializer):
